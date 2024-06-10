51650e2a96fe7d1f5fef462db88ac3a4e54f65a8
Wikan <wikan25@gmail.com>
2024-06-10 22:45:06 +0700

    fix: linting inject.js

--- src/content/inject.js
+++ src/content/inject.js
@@ -1,21 +1,23 @@
-const addCustomStyle = css => parentPH.appendChild(document.createElement('style')).innerHTML = css;
+function addCustomStyle(css) {
+  return parentPH.appendChild(document.createElement('style')).innerHTML = css;
+}
 
-const elFactory = (type, attributes, ...children) => {
-  const el = document.createElement(type)
+function elFactory(type, attributes, ...children) {
+  const el = document.createElement(type);
 
   for (key in attributes) {
-    el.setAttribute(key, attributes[key])
+    el.setAttribute(key, attributes[key]);
   }
 
   children.forEach(child => {
     if (typeof child === 'string') {
-      el.appendChild(document.createTextNode(child))
+      el.appendChild(document.createTextNode(child));
     } else {
-      el.appendChild(child)
+      el.appendChild(child);
     }
-  })
+  });
 
-  return el
+  return el;
 }
 
 var parentPH = elFactory(
