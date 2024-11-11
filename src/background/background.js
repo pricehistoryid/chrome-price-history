932becc2f175e33a9b20fa770badd77de4d67626
Wikan <wikan25@gmail.com>
2024-11-11 18:43:02 +0700

    fix: tokopedia listen on url change on specific url pattern

--- src/background/background.js
+++ src/background/background.js
@@ -1,17 +1,17 @@
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  if (request.type === "getProfileUserInfo") {
-    chrome.identity.getProfileUserInfo(function (userInfo) {
-      sendResponse({
-        id: userInfo.id,
-        email: userInfo.email
-      });
-    });
-    return true;
-  }
-});
+// chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+//   if (request.type === "getProfileUserInfo") {
+//     chrome.identity.getProfileUserInfo(function (userInfo) {
+//       sendResponse({
+//         id: userInfo.id,
+//         email: userInfo.email
+//       });
+//     });
+//     return true;
+//   }
+// });
 
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-  if (changeInfo.url) {
+  if (changeInfo.url && changeInfo.url.match('https:\/\/.*.tokopedia.com\/.*\/.*')) {
     if (changeInfo.url.indexOf("#") === -1) {
       let url = changeInfo.url;
       url = url.replace(/^https?:\/\//, '');
